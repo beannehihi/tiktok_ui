d3de47528c8238aa74bf4425e182315fa4efa01e
nguyenan <vangtienchi@gmail.com>
2024-03-02 23:45:22 +0700

    setUp

---
+++ src/MyAlert.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyAlert = () => {
+  return alert("say hi");
+};
+
+export default MyAlert;
---
+++ src/component/button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MyAlert from "~/MyAlert";
+
+const Button = () => {
+  return (
+    <div>
+      <button onClick={MyAlert}>Click Me!</button>
+    </div>
+  );
+};
+
+export default Button;
